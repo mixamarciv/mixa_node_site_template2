422ad9b16598743d1881c887fbcc9a2f5e1d6847
mixa marciv <mixamarciv@gmail.com>
2014-07-25 17:38:49 +0400

    update client js for send query

--- public/templates/default/scripts/post.search.js
+++ public/templates/default/scripts/post.search.js
@@ -53,77 +53,80 @@ example:
   send_ajax_query_timeout("test_11",ajax_options,500);
 ***************************/
 function send_ajax_query_timeout(id_user_query,options,timeout){
-  if(typeof timeout == 'undefined' || timeout <= 10 ){
+  if(!timeout || timeout <= 10 ){
     timeout = 10;
   }
   
-  if(typeof options == 'undefined'){
+  if(!options){
     alert("незадан параметр 'options' для 'send_ajax_query_timeout(id_user_query,options,timeout)' !");
     return 0;
   }
   
-  if(typeof options.onSend !== 'undefined'){
+  if(options.onSend && jQuery.isFunction(options.onSend)){
     options.onSend();
   }
-
-  if(typeof window['ajax_query'] == 'undefined'){
-    window['ajax_query'] = {};
+  
+  if(!window.list_ajax_query){
+    window.list_ajax_query = {};
   }
-  if(typeof window['ajax_query'][id_user_query] == 'undefined'){
-    window['ajax_query'][id_user_query] = {};
+  var aq = window.list_ajax_query;  //набор пользовательских запросов
+  
+  
+  if(!aq.id_user_query){
+    aq[id_user_query] = {};
+  }
+  var uq = aq[id_user_query];  //пользовательский запрос
+  
+  uq.options = options;
+  if(!uq.status){
+    uq.status = {result_wait: 0, restart_query: 0, last_params: null, load_count: 0};
   }
   
-  window['ajax_query'][id_user_query]['options'] = options;
-  
-  if(typeof window['ajax_query'][id_user_query]['status'] == 'undefined'){
-    window['ajax_query'][id_user_query]['status'] = {result_wait: 0, restart_query: 0, last_params: null, load_count: 0};
-  }
-  
-  var s = window['ajax_query'][id_user_query];
-  if( s['status']['result_wait'] == 1){
-    if(s['status']['restart_query'] == 0){
-        s['status']['restart_query'] = 1;
+  var ss = uq.status;
+  if( ss.result_wait == 1){
+    if(ss.restart_query == 0){
+        ss.restart_query = 1;
     }
-    if(typeof options.onWait !== 'undefined'){
+    if(options.onWait && jQuery.isFunction(options.onWait)){
       options.onWait();
     }
     return 0;
   }
   
-  s['status']['result_wait'] = 1;
+  ss.result_wait = 1;
 
-  setTimeout(function(){
-    var options_new = my_clone(window['ajax_query'][id_user_query]['options']);
-    if(typeof options_new['complete'] !== 'undefined'){ // && jQuery.isFunction(options['complete'])
-        //alert("ВНИМАНИЕ: AJAX параметр options['complete'] будет проигнорирован!");
-	window['ajax_query'][id_user_query]['options']['my_ajax_on_complete'] = options_new['complete'];
-
-	options['complete'] = null;
-        delete options['complete'];
+    var options_new = my_clone(uq.options);
+    if( options_new.complete ){ // && jQuery.isFunction(options['complete'])
+	  uq.options.user_ajax_on_complete = options_new['complete'];
+	  options['complete'] = null;
+	  delete options['complete'];
     }
     options_new.complete = function(jqXHR,status){
-	    if(typeof window['ajax_query'][id_user_query]['options']['my_ajax_on_complete'] !== 'undefined'){
-		window['ajax_query'][id_user_query]['options']['my_ajax_on_complete'](jqXHR,status);
+	    if(uq.options.user_ajax_on_complete && jQuery.isFunction(uq.options.user_ajax_on_complete) ){
+		uq.options.user_ajax_on_complete(jqXHR,status);
 	    }
 	    if(status!="success"){
 		var msg = status+" : ["+jqXHR['status']+"] "+jqXHR['statusText'];
 		alert("ОШИБКА AJAX загрузки: \n"+msg+"\nurl:"+options_new['url']+"\ndata:"+var_dump(options_new['data']));
 	    }
-	    var s = window['ajax_query'][id_user_query];
-	    window['ajax_query'][id_user_query]['status']['result_wait']=0;
-	    if(window['ajax_query'][id_user_query]['status']['restart_query']==1){
-		window['ajax_query'][id_user_query]['status']['restart_query']=0;
+	    
+	    if(ss.restart_query==1){
+		ss.restart_query = 0;
 		setTimeout(function(){
-		  window['send_ajax_query_timeout'](id_user_query,window['ajax_query'][id_user_query]['options'],timeout);
-		},timeout);
+		  send_ajax_query_timeout(id_user_query,window['list_ajax_query'][id_user_query]['options'],timeout);
+		},10);
+	    }else{
+		ss.result_wait = 0;
+		if(uq.options.on_end_all && jQuery.isFunction(uq.options.on_end_all) ){
+		    uq.options.on_end_all(jqXHR,status);
+		}
 	    }
-	};
+    };
     //alert('send ajax: ['+options_new.type+'] '+options_new.url);
-    $.ajax(options_new)
+    $.ajax(options_new);
     //.done(options_new.success)
     //.always(function(){ alert( "complete" ); });
 
-  },100);
   return 1;
 }
 
@@ -152,8 +155,12 @@ function post_search__run_query() {
                 alert('not found object #search_data');
               }
               obj.html(html);
-              load_post__progressbar(0);
           },
+	  on_end_all: function(d,status){
+	      setTimeout(function(){
+		  load_post__progressbar(0);
+	      },100);
+	  },
           test: 0
     };
     
@@ -176,14 +183,14 @@ function post_search__get_form_data(form) {
 
 function load_post_search(form) {
     load_post__progressbar(0);
-    form.submit(function(event){
-        post_search__run_query();
-        event.preventDefault();
-    });
+    //form.submit(function(event){
+    //    post_search__run_query();
+    //    event.preventDefault();
+    //});
     var obj_input_text = form.find('#search');
-    obj_input_text.change(function(){
-        post_search__run_query();
-    });
+    //obj_input_text.change(function(){
+    //    post_search__run_query();
+    //});
     obj_input_text.keyup(function(){
         post_search__run_query();
     });
